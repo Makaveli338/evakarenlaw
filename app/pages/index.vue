15486d754fcdf9d6768a173b19cf56f9ee583a95
Intern <intern2@valuechainfactory.com>
2026-02-04 11:31:46 +0300

    Hover text

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -388,7 +388,7 @@
           >
             <h3 class="text-xl font-semibold mb-2">{{ template.title }}</h3>
             <p class="text-muted text-sm mb-4">{{ template.description }}</p>
-            <span class="text-accent text-sm font-mono flex gap-2 items-center"
+            <div class="text-black hover:text-[#c9a87c] text-sm font-mono flex gap-2 items-center"
               >Preview & Download
 
               <svg
@@ -405,7 +405,7 @@
                   d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
                 />
               </svg>
-            </span>
+            </div>
           </div>
         </div>
       </div>
